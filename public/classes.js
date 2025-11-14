1c15ddf046c480aaf965205c35d613077fd122af
Vasileios Parikoglou <vparik@192.168.1.12>
2025-11-14 20:46:29 +0200

    fix a typo

--- public/classes.js
+++ public/classes.js
@@ -1,6 +1,6 @@
 import { ExplosionSound } from './audio.js';
 import { entities, bricks, COLS, ROWS, updateTileMap2D } from './bomber.js';
-import { addPausedDuration } from './gameloop.js';
+import { addPausedDuration } from './gameLoop.js';
 import { addScore } from './gameState.js';
 
 
